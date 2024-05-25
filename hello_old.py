4d220367b847626eed01e2b62f83d5a483c1f5a5
riccardodiotti <164208949+riccardodiotti@users.noreply.github.com>
2024-05-25 15:31:55 +0200

    Add files via upload

--- hello_old.py
+++ hello_old.py
@@ -1,4 +1,3 @@
-from datetime import timedelta
 import os
 from enum import Enum, unique
 from flask import Flask, render_template, redirect, request,g,session, render_template_string,flash
@@ -10,25 +9,26 @@ from sqlalchemy.sql import func
 from flask_mysqldb import MySQL
 import mysql.connector
 import sqlite3
+import subprocess
 from glassdoor_employee_scraper import *
-from sqlalchemy import text
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE = os.path.join(BASE_DIR, "database.db")
 app = Flask(__name__)
-#app.config['MYSQL_HOST'] = 'localhost'
-#app.config['MYSQL_USER'] = 'root'
-#app.config['MYSQL_PASSWORD'] = '1234'
-#app.config['MYSQL_DB'] = 'database'
+#oldsqllitedb
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'database'
+#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql///root:1234@localhost/database"
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "Quasimodo12"
-#db = MySQL(app)
-database_url = 'postgresql://u8ok4t688g05br:p983f0fd07ee81aadcaa0cd4bb735b63b243045c07bf1b8ba49607dcc79129a53@c97r84s7psuajm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d8p6bnpqnneug2'
-#database_url = 'postgresql://postgres:1234@localhost:5432/database'
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
+db = MySQL(app)
 
 choices = []
+#with open("list-of-universities.txt", "r", encoding="utf8") as universities_list:
 with open("list-of-jobs.txt", "r", encoding="utf8") as jobs_list:
 	lines = jobs_list.readlines()
 	for l in lines:
@@ -65,13 +65,16 @@ class RequiredIf(DataRequired):
             super(RequiredIf, self).__call__(form, field)
 
 class SearchForm(FlaskForm):
-    job = SelectField("Job Search List", choices=choices, validators=[DataRequired()])
+    #university = SelectField("University Search", choices=choices, validators=[DataRequired()], validate_choice=True, default='Polytechnic Institute of Milan')
+    #job = SelectField("Job Search List", choices=choices, validators=[DataRequired()], validate_choice=False, default='Project Manager')
+    job = SelectField("Job Search List", choices=choices)
     job_details = StringField('Job Search text',
         validators=[
             RequiredIf('job', 'Other')
         ]
     )
-    country = SelectField("Country Search List", choices=countries, validators=[DataRequired()])
+    #country = SelectField("Country Search List", choices=countries, validators=[DataRequired()], validate_choice=False, default='United States')
+    country = SelectField("Country Search List", choices=countries)
     country_details = StringField('Country Search text',
         validators=[
             RequiredIf('country', 'Other')
@@ -86,33 +89,29 @@ class SearchForm(FlaskForm):
         return True
 
 def search_database(searchkey1,searchkey2):
-    conn = db.engine.connect()
-    #cursor = db.connection.cursor()
-    #cursor = conn.cursor()
+    #conn = mysql.connector.connect(
+    #   host="localhost",
+    #    user="root",
+    #   passwd="1234"
+    #)
+    cursor = db.connection.cursor()
+    #print("Search key is"+searchkey1)
     searchkeystr1 = str(searchkey1)
     searchkeystr2 = str(searchkey2)
-    result = conn.execute(
-            text("SELECT * FROM public.results WHERE Job = :job AND Location = :location"),
-            {'job': searchkeystr1, 'location': searchkeystr2}
-        )
-    #cursor.execute("""SELECT * FROM results WHERE Job = %s AND Location = %s""",(searchkeystr1,searchkeystr2,))
-    #msg = cursor.fetchone() 
-    #results = cursor.fetchall()
-    results = result.fetchall()
-    #cursor.execute("""SELECT * FROM salary_employers WHERE Job = %s AND Location = %s""",(searchkeystr1,searchkeystr2,))
-    salary_employers = conn.execute(
-            text("SELECT * FROM public.salary_employers WHERE Job = :job AND Location = :location"),
-            {'job': searchkeystr1, 'location': searchkeystr2}
-        )
-    conn.close()
-    #cursor.close()
+    cursor.execute("""SELECT * FROM results WHERE Job = %s AND Location = %s""",(searchkeystr1,searchkeystr2,))
+    msg = cursor.fetchone() 
+    if not msg:
+        subprocess.run(["python", "glassdoor_employee_scraper.py"])
+    results = cursor.fetchall()
+    cursor.execute("""SELECT * FROM salary_employers WHERE Job = %s AND Location = %s""",(searchkeystr1,searchkeystr2,))
+    salary_employers = cursor.fetchall()
+    #formatted_results = [[f'{result[0]:,}'] for result in results]
+    cursor.close()
     return results, salary_employers
 
 @app.route('/',methods = ['GET','POST'])
-@app.route('/home',methods = ['GET','POST'])
 def index():
     form = SearchForm()
-    session.permanent=True
     if request.method == 'GET' and request.args.get('job') and request.args.get('country'):
         jobsearch = request.args.get('job')
         countrysearch = request.args.get('country')
@@ -120,9 +119,9 @@ def index():
             jobsearch = request.args.get('job_details')
         if request.args.get('country') == "Other":
             countrysearch = request.args.get('country_details')
+        #session['university'] =  request.args.get('university')
         session['job'] =  jobsearch
         session['country'] =  countrysearch
-        #flash("Search Request Submitted Successfully")
         return redirect('/search')
     else:
         return render_template("index.html",form=form)
@@ -130,47 +129,17 @@ def index():
 if __name__ == "__main__":
     app.run(debug=True)
 
-@app.route('/search')
-def search():
-    searchkey1 = session.get('job', None)
-    searchkey2 = session.get('country', None)
-    conn = db.engine.connect()
-    #cursor = db.connection.cursor()
-    searchkeystr1 = str(searchkey1)
-    searchkeystr2 = str(searchkey2)
-    print(searchkey1)
-    print(searchkey2)
-    #cursor.execute("""SELECT * FROM results WHERE Job = %s AND Location = %s""",(searchkeystr1,searchkeystr2,))
-    result = conn.execute(
-            text("SELECT * FROM public.results WHERE job = :job AND location = :location"),
-            {'job': searchkeystr1, 'location': searchkeystr2}
-        )
-    #msg = cursor.fetchone() 
-    msg=result.fetchone() 
-    if not msg:
-        display = exec(searchkeystr1,searchkeystr2)
-        flash(display,"info")
-        return redirect('/')
-    else:
-        flash("Search Request Submitted Successfully")
-    rows_results, rows_salaries = search_database(searchkey1,searchkey2)
-    return render_template("results.html",rows_results = rows_results,rows_salaries = rows_salaries,job_description = searchkeystr1,  location=searchkeystr2)
-
-@app.route("/logout")
-def logout():
-     session.pop("user",None)
-     flash("You have been logged out!","info")
-     return redirect('/')
-    
-@app.errorhandler(500)
-def internal_error(error):
-    return "500 error"
-
-@app.errorhandler(404)
-def pageNotFound(error):
-    return "page not found"
 
 class Results():
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    university = db.Column(db.String(200), nullable = False)
+    job = db.Column(db.String(200), nullable = False)
+    company = db.Column(db.String(200), nullable = False)
+    avgsalary = db.Column(db .String(200), nullable = False)
+    salaryrange = db.Column(db.String(200), nullable = False)
+    date_added = db.Column(db.DateTime, nullable = False)
+    '''
     def __init__(self, id, name, university, job, company, avgsalary, salaryrange, date_added):
         self.id = id
         self.name = name
@@ -183,15 +152,40 @@ class Results():
 
     def save(self):
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO public.results (name, university, job, company) VALUES (%s, %s, %s, %s)", 
+        cur.execute("INSERT INTO results (name, university, job, company) VALUES (%s, %s, %s, %s)", 
                     (self.name, self.university, self.job, self.company))
         mysql.connection.commit()
         cur.close()
 
     def fetch_all():
         cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM results.results")
+        cur.execute("SELECT * FROM results")
         data = cur.fetchall()
         cur.close()
         return data
+
+@app.route('/search')
+def search():
+    #print(session.get('university', None))
+    #print(session.get('job', None))
+    #print(session.get('country', None))
+    #searchkey = session.get('university', None)
+    searchkey1 = session.get('job', None)
+    searchkey2 = session.get('country', None)
+    cursor = db.connection.cursor()
+    #print("Search key is"+searchkey1)
+    searchkeystr1 = str(searchkey1)
+    searchkeystr2 = str(searchkey2)
+    print(searchkey1)
+    print(searchkey2)
+    cursor.execute("""SELECT * FROM results WHERE Job = %s AND Location = %s""",(searchkeystr1,searchkeystr2,))
+    msg = cursor.fetchone() 
+    if not msg:
+        exec(searchkeystr1,searchkeystr2)
+        flash(f"Record not yet existing! Please return to Home and run the search again", category="error")
+        return redirect('/')
+    rows_results, rows_salaries = search_database(searchkey1,searchkey2)
+    return render_template("results.html",rows_results = rows_results,rows_salaries = rows_salaries)
+
+    
 

9213901b6285434d0611a0cf9bce0685521e417c
riccardodiotti <164208949+riccardodiotti@users.noreply.github.com>
2024-06-02 21:25:11 +0200

    Update glassdoor_employee_scraper.py

--- glassdoor_employee_scraper.py
+++ glassdoor_employee_scraper.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import Keys, ActionChains
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException

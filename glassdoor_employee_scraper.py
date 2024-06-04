faedaefcc6dbde3c0b7aea537fa3d0f2be9d0c82
riccardodiotti <164208949+riccardodiotti@users.noreply.github.com>
2024-06-04 19:29:08 +0200

    Update glassdoor_employee_scraper.py

--- glassdoor_employee_scraper.py
+++ glassdoor_employee_scraper.py
@@ -141,6 +141,8 @@ def getSalaries(driver,keyword,location):
 def exec(keyword,location):
     service = Service(executable_path='./chromedriver.exe')
     options = Options()
+    options.add_argument('--headless')
+    options.add_argument('start-maximized')
     #options.add_argument('--no-sandbox')
     #options.add_argument('--ignore-certificate-errors')
     #options.add_argument('--disable-dev-shm-usage')
